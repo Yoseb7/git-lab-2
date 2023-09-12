fa2325dea97154933ec12b584dac5073e7767a0d
Yoseb7 <sw969822@ohio.edu>
2023-09-12 16:17:47 -0400

    a

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Yoseb Choi
+ *        Date: September 11, 2023
+ * Description: Lab #2, program that prints the line "Hello Git!!"
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]) {
+     
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }
